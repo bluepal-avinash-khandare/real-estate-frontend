41930e00a2e8fa10a6b37d6a3c14bab8e1604dc4
Avinash <beinga2k1@gmail.com>
2025-09-24 00:29:24 +0530

    modified sidebar and footer

--- src/components/role-based/CustomerSidebar.jsx
+++ src/components/role-based/CustomerSidebar.jsx
@@ -137,7 +137,7 @@ const CustomerSidebar = ({ isExpanded }) => {
   const icons = {
     dashboard: (
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v-4a1 1 0 00-1-1h-4m-6 0a1 1 0 00-1 1v4m0 0V6a1 1 0 001 1m0 0V4a1 1 0 001 1m0 0V4a1 1 0 001 1m0 0V4a1 1 0 001 1" />
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v-4a1 1 0 00-1-1h-4m-6 0a1 1 0 00-1 1v4m0 0V6a1 1 0 001 1m0 0V4a1 1 0 001 1m0 0V4a1 1 0 001 1" />
       </svg>
     ),
     properties: (
@@ -234,4 +234,3 @@ const CustomerSidebar = ({ isExpanded }) => {
 };
 
 export default CustomerSidebar;
-
