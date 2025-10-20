bbff311bca2dc85dee9b26caec4ec3b782babc54
Avinash <beinga2k1@gmail.com>
2025-10-20 16:05:45 +0530

    update Contact, and footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -155,7 +155,7 @@ const Footer = () => {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                     </svg>
                   </div>
-                  <span className="text-gray-400 text-base">Shiv bagh,Ameerpet,Hyderabad 500012</span>
+                  <span className="text-gray-400 text-base">Bluepal It Solution,Kukatpally,Hyderabad 500042</span>
                 </li>
               </ul>
             </div>
